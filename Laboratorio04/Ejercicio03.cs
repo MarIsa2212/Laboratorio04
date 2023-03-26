1400e39a18f1ccf3839b2095fc3207626438fb5e
María Isabel Martínez Del cid <mimartinezd@correo.url.edu.gt>
2023-03-25 23:01:19 -0600

    Resolver Ejercicio 4

--- Laboratorio04/Ejercicio03.cs
+++ Laboratorio04/Ejercicio03.cs
@@ -20,6 +20,23 @@ namespace Laboratorio04
             }
 
             string result = "";
+
+            int i = num1.Length - 1, j = num2.Length - 1; // .Length cantidad compuesta del string
+            int acarreo = 0; //acarreo: transportar un dígito que se transfiere de una columna de dígitos a otra columna de dígitos más significativos.
+
+            while (i >= 0 && j >= 0){
+                int r = acarreo + (num1[i] - '0') + (num2[j] - '0');
+                acarreo = r / 10;
+
+                result = (r % 10) + result;
+
+                i--;
+                j--;
+            }
+            if (acarreo > 0)
+                result = acarreo + result;
+
+            return result;
         }
     }
 }
